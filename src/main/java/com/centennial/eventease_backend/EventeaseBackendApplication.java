b0a2916a475c70b6fa8ce668c84c621157abf2e9
Carlos Rosario <crosari5@my.centennialcollege.ca>
2025-02-17 10:14:36 -0500

    Setting up the project

---
+++ src/main/java/com/centennial/eventease_backend/EventeaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.centennial.eventease_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventeaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventeaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centennial/eventease_backend/EventeaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centennial.eventease_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventeaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
